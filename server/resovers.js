5f7d0f0e5a4ec3355949b7f9f6e50dc037148b76
Adilson Fuxe <adilson.fuxe@bancobai.ao>
2023-07-05 15:57:10 +0100

    feat: add new queries

--- server/resovers.js
+++ server/resovers.js
@@ -1,7 +1,16 @@
-import {getJobs} from "./db/jobs.js";
+import {getJob, getJobs} from "./db/jobs.js";
+import {getCompany} from "./db/companies.js";
 
 export const resolvers = {
   Query: {
-    jobs:  () =>  getJobs()
+    job: (_root, args) => getJob(args.id),
+    jobs:  () =>  getJobs(),
+    company: (__root, args) => getCompany(args.id)
+  },
+  Job: {
+    date: ({createdAt}) => toIsoDate(createdAt),
+    company: ({companyId}) => getCompany(companyId)
   }
 }
+
+const toIsoDate = x => x.slice(0, 'yyyy-mm-dd'.length)
