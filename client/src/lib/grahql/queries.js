27d87650a1abfaac383487c5584dab876d70f5c6
Adilson Fuxe <adilson.fuxe@bancobai.ao>
2023-07-10 14:21:46 +0100

    feat: request from graphql client

--- client/src/lib/grahql/queries.js
+++ client/src/lib/grahql/queries.js
@@ -1,48 +1,75 @@
-import {GraphQLClient, gql} from 'graphql-request'
+import {ApolloClient, InMemoryCache, gql, createHttpLink, ApolloLink, concat} from '@apollo/client';
+import {GraphQLClient} from 'graphql-request'
 import {getAccessToken} from "../auth";
 
-const client = new GraphQLClient('http://localhost:9000/graphql', {
-  headers: () => {
-    const accessToken = getAccessToken();
-    if(accessToken) {
-      return {'Authorization': `Bearer ${accessToken}`}
-    }
-    return {};
+// const client = new GraphQLClient('http://localhost:9000/graphql', {
+//   headers: () => {
+//     const accessToken = getAccessToken();
+//     if (accessToken) {
+//       return {'Authorization': `Bearer ${accessToken}`}
+//     }
+//     return {};
+//   }
+// });
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:9000/graphql'
+})
+
+const authLink= new ApolloLink((operation, forward) => {
+  const accessToken = getAccessToken();
+  if(accessToken) {
+    operation.setContext({
+      headers: {'Authorization': `Bearer ${accessToken}`}
+    })
   }
+  return forward(operation);
+})
+
+const apolloClient = new ApolloClient({
+  link: concat(authLink, httpLink),
+  cache: new InMemoryCache()
 });
 
-export const createJob = async ({title,description}) => {
-  const query = gql`
+export const createJob = async ({title, description}) => {
+  const mutation = gql`
       mutation CreateJob($input: CreateJobInput!)  {
           job: createJob(input: $input) {
               id
           }
       }
   `
-
-  const {job} =  await  client.request(query, { input: {title, description} });
-  return job;
+  const {data} = await apolloClient.mutate({
+    mutation,
+    variables: {
+      input: {
+        title,
+        description
+      }
+    }
+  })
+  return data.job;
 }
 
 export const getJobs = async () => {
   const query = gql`
-    query Query {
-      jobs {
-        id,
-        title,
-        date,
-        company {
-          id,
-          name
-        }
-      } 
-    }
+      query Query {
+          jobs {
+              id,
+              title,
+              date,
+              company {
+                  id,
+                  name
+              }
+          }
+      }
   `
-  const { jobs } = await client.request(query);
-  return jobs
+  const {data} = await apolloClient.query({query})
+  return data.jobs;
 }
 
-export  const getJob = async (id) => {
+export const getJob = async (id) => {
   const query = gql`
       query Job($id: ID!) {
           job(id: $id) {
@@ -57,11 +84,15 @@ export  const getJob = async (id) => {
           }
       }
   `
-  const {job} =  await  client.request(query, { id });
-  return job;
+  const {data} = await apolloClient.query({
+    query, variables: {
+      id
+    }
+  })
+  return data.job;
 }
 
-export  const getCompany = async (id) => {
+export const getCompany = async (id) => {
   const query = gql`
       query Job($id: ID!) {
           company(id: $id) {
@@ -77,6 +108,6 @@ export  const getCompany = async (id) => {
           }
       }
   `
-  const { company } = await client.request(query, { id });
-  return company;
+  const {data} = await apolloClient.query({query, variables: {id}})
+  return data.company;
 }
