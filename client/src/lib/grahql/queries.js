a5689eb105143879f35c3bb9adea6df720cf36c4
Adilson Fuxe <adilson.fuxe@bancobai.ao>
2023-07-10 15:02:05 +0100

    feat: cache with apollo client

--- client/src/lib/grahql/queries.js
+++ client/src/lib/grahql/queries.js
@@ -1,16 +1,5 @@
 import {ApolloClient, InMemoryCache, gql, createHttpLink, ApolloLink, concat} from '@apollo/client';
-import {GraphQLClient} from 'graphql-request'
 import {getAccessToken} from "../auth";
-
-// const client = new GraphQLClient('http://localhost:9000/graphql', {
-//   headers: () => {
-//     const accessToken = getAccessToken();
-//     if (accessToken) {
-//       return {'Authorization': `Bearer ${accessToken}`}
-//     }
-//     return {};
-//   }
-// });
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:9000/graphql'
